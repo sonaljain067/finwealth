11a14f5ce3c1d4645170a27a5e55ef2260d9967e
sonaljain067 <52230497+sonaljain067@users.noreply.github.com>
2021-04-18 13:29:57 +0530

    Add files via upload

---
+++ client/src/signup.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const signup = () => {
+    return (
+        <div className="signup">
+            <h1>SignUp</h1>
+            <input type="text" placeholder=""
+        </div>
+    )
+}
